651c844c1c8ee48ed52d843d9406ec6e9643aef1
chotung <choelestung@gmail.com>
2019-01-06 04:02:27 -0500

    added stock card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Search from './components/Search';
+import StockCard from './components/StockCard';
 
 class App extends Component {
   state = {
@@ -13,7 +14,12 @@ class App extends Component {
     ],
     term: '',
     filtered: [], 
-    stocks: []
+    stocks: [],
+    user: {
+      userName: '',
+      password: '',
+      portfolio: [{}],
+    }
   }
 
   handleChange = (e) => {
@@ -23,10 +29,7 @@ class App extends Component {
   }
   
   filterResults = () => {
-    return this.state.suggestion.map(suggestion => {
-      return suggestion.label.includes(this.state.term)
-      
-    })
+
   }
   
 
@@ -35,15 +38,24 @@ class App extends Component {
     // console.log(this.state.term)
 
     return <div className="App">
-        {this.filterResults()}
-        <Search handleChange={this.handleChange} searchTerm={this.state.term} />
+      <StockCard />
+        {/* {this.filterResults()} */}
+        {/* <Search handleChange={this.handleChange} searchTerm={this.state.term} /> */}
       </div>;
   }
 }
 
 export default App;
 
+// Pseudo code
 
-// { symbol: "APPL", companyName: "Apple Inc.", sector: "Technology", open: "154", change: "-1.67", changePercent: "-0.01158" },
-// { symbol: "AMZN", companyName: "Amazon", sector: "Technology", open: "154", change: "-1.67", changePercent: "-0.01158" },
-// { symbol: "BABA", companyName: "Alibaba Group Holding Ltd", sector: "Technology", open: "154", change: "-1.67", changePercent: "-0.01158" },
+// types something into input field
+// changes term
+
+// filtered state gets changed
+
+// where does filtered state get the information?
+// gets data from "suggestions"
+// filter suggestions based off the term 
+
+//  then set the state of filtered to whatever the filtered results was
---
+++ src/components/StockCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function StockCard() {
+  return (
+    <div>
+      <h1>Stock Name</h1>
+      <span>current price</span>
+      <span>net change</span>
+      <a href="#">more details</a>
+      <p>add to portfolio</p>
+    </div>
+  )
+}
+
+export default StockCard
