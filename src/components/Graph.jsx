5d845f926f91ef74eb49ecc2c7ba419c703c8d04
chotung <choelestung@gmail.com>
2019-01-10 21:01:49 -0500

    added a bar graph

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
+import * as d3 from 'd3'
 
-function Graph() {
-  return (
-    <div>
-      <h2>Graph</h2>
-    </div>
-  )
+export default class Graph extends Component {
+
+componentDidMount = () => {
+  this.drawChart()
 }
 
-export default Graph
+drawChart = () => {
+  const data = [12, 5, 6, 6, 9, 10];
+
+  const svg = d3.select("body").append("svg").attr("width", 700).attr("height", 300);
+  svg
+    .selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", (d, i) => i * 70)
+    .attr("y", 0)
+    .attr("width", 25)
+    .attr("height", (d, i) => d)
+    .attr("fill", "green");
+}
+
+
+  render() {
+    return (
+      <div id={"#" + this.props.id}></div>
+    )
+  }
+}
